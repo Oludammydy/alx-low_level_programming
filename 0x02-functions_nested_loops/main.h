92f4caabc6a934d7169875e408c8f97d0672f7f1
oludammydy <oludammydy@gmail.com>
2022-03-16 05:55:08 +0100

    C-Functions

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,25 +1,22 @@
-int _putchar(char);
+#include "main.h"
 
-void print_alphabet(voin);
+/**
+ * main - prints _putchar
+ *
+ * Return: 0 (Success)
+ */
 
-int _abs(int n);
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-int print_last_digit(int);
-d);
-
-void print_alphabet_x10(void);
-
-int _islower(int c);
-
-int _isalpha(int c);
-
-int print_sign(int 
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int, int);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
+	return (0);
+}
