516a17a20cc14f558539b1728205b9ad0cb24318
oludammydy <oludammydy@gmail.com>
2022-03-16 06:15:03 +0100

    C-Functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * main - prints _put
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');char
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+int main(void)
+{
+	char *ch = "_putchar";
+
+	while (*ch)
+	{
+		_putchar(*ch);
+		ch++;
+	}
+	_putchar('\n');
+return (0);
+}
