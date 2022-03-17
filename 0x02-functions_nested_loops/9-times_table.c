77fb04f9ec81fe1380df0e5ff91d423041412da1
oludammydy <oludammydy@gmail.com>
2022-03-17 18:58:57 +0100

    C-Functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,36 +1,39 @@
 #include "main.h"
 /**
-* times_table -  a function that prints the 9 times table, starting with 0
-* rone = row, cone = column, d = digits of cu{
-int r, c, d;
-for (r = 0; r <= 9; r++)
-{
-_purrent result
-* Return: times table
-* add extra space past single digit
-*/
+ * times_table -  a furrent result
+ * Return: times table
+ * add extra snction that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of cupace past single digit
+ */
 void times_table(void)
-tchar('0');
-_putchar(',');
-_putchar(' ');
-for (c = 1; c <= 9; c++)
 {
-d = (r * c);
-if ((d / 10) > 0)
-{
-_putchar((d / 10) + '0');
-}
-else
-{
-_putchar(' ');
-}
-_putchar((d % 10) + '0');
-if (c < 9)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-_putchar('\n');
-}
+int r, c, d;
+
+	for (r = 0; r <= 9; r++)
+	{
+		_pu10) > 0)
+	{
+		_putchar((d / 10) + '0');
+	}
+	else
+	tchar('0');
+		_putchar(',');
+		_putchar(' ');
+	for (c = 1; c <= 9; c++)
+	{
+		d = (r * c);
+	if ((d / {
+		_putchar(' ');
+	}
+		_putchar((d % 10) + '0');
+
+	if (c < 9)
+	{
+		_putchar(',');
+	      _putchar(' ');
+	}
+	}
+	_putchar('\n');
+	}
+
 }
