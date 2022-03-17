29f2f8703bf225e4cb55e5c5a0fb5c274bb0b7b3
oludammydy <oludammydy@gmail.com>
2022-03-17 18:49:08 +0100

    C-Functions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,15 +7,15 @@
 */
 void jack_bauer(void)
 {
-iputchar(i % 10 + 48);
-_putchar(':');
-_putchant i, j;
+int i, j;
 for (i = 0; i < 24; i++)
 {
 for (j = 0; j < 60; j++)
 {
 _putchar(i / 10 + 48);
-_r(j / 10 + 48);
+_putchar(i % 10 + 48);
+_putchar(':');
+_putchar(j / 10 + 48);
 _putchar(j % 10 + 48);
 _putchar('\n');
 }
