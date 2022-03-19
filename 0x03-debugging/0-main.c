69ea3c28e307c8172d2c22edea4a22d1888e6eb1
Oluwadamilola Olaniyi <96773767+Oludammydy@users.noreply.github.com>
2022-03-19 13:03:56 +0100

    Update and rename 0-main.c, to 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ *main - Debugging.
+ *
+ *Return: 0
  */
 
 int main(void)
@@ -11,6 +12,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
